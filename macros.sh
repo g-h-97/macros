da801324bb72b9ff4b30c18ee9b045600e519e5f
g_h_97 <hguenfaf@pm.me>
2024-06-02 08:38:15 +0100

    removed sleep

--- macros.sh
+++ macros.sh
@@ -4,7 +4,7 @@ confDir=~/.config/macros
 #macro=$(echo "rm_sec" | bemenu)
 macro=$(ls ${confDir} | bemenu)
 
-sleep 2 && ydotool type -f ${confDir}/${macro}
+ydotool type -f ${confDir}/${macro}
 
 #case $macro in
 #	"rm_sec") sleep 2 && ydotool type -f - <<< $(spitmacro $macro)
